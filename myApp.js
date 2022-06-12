2de48251cb4074c6b7d7895072614d4435a0c70a
Spencer Harris <morrisonleary@gmail.com>
2022-06-12 12:08:40 -0400

    added comment about function

--- myApp.js
+++ myApp.js
@@ -1,11 +1,12 @@
-
 let express = require('express');
 let app = express();
 
 console.log('Hello World');
 
-app.get('/', (req, res) => {
-    res.send(path.join(__dirname, + ''));
+//this needs an function, not arrow function to work
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 module.exports = app;
