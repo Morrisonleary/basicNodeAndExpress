269b6ae692245915eb7f3534307fba302fcd5a08
Spencer Harris <morrisonleary@gmail.com>
2022-06-12 12:54:18 -0400

    finished challenge 6

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 console.log('Hello World');
 
@@ -12,7 +13,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/json', (req, res) => {
-    res.json({ message: "Hello json" })
+    // res.json({ message: "Hello json" })
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({ message: "Hello json".toUpperCase() })
+    } else {
+        res.json({ message: "Hello json" })
+    }
+
 })
+
+console.log(process.env.MESSAGE_STYLE)
 
 module.exports = app;
