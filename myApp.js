9a16b0900e6f056d969f67193ec5a15c2d06656f
Spencer Harris <morrisonleary@gmail.com>
2022-06-12 12:26:18 -0400

    fixed challenge 3 syntax, updated challenge 5

--- myApp.js
+++ myApp.js
@@ -7,9 +7,7 @@ console.log('Hello World');
 
 app.use('/public', express.static(__dirname + '/public'));
 
-//this needs an function, not arrow function to work
-
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
