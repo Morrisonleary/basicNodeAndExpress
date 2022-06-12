35b038ecbbe3a9d776dd2728c10dc079fd28ccd7
Spencer Harris <morrisonleary@gmail.com>
2022-06-12 12:24:05 -0400

    finished challenge 5

--- myApp.js
+++ myApp.js
@@ -3,14 +3,18 @@ let app = express();
 
 console.log('Hello World');
 
+//serves css file
 
 app.use('/public', express.static(__dirname + '/public'));
-
 
 //this needs an function, not arrow function to work
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/json', (req, res) => {
+    res.json({ message: "Hello json" })
+})
 
 module.exports = app;
